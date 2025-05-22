2a8a35f8362a6cee3ecfd7adf870dcd806ec3281
quandohong109 <quandohong109@gmail.com>
2025-05-22 21:17:39 +0700

    clean interfaces

--- lib/objects/voucher_related/percentage_interface.dart
+++ lib/objects/voucher_related/percentage_interface.dart
@@ -1,15 +1,4 @@
-import 'package:flutter/cupertino.dart';
-
-import '../../widgets/general/app_text_style.dart';
-
 abstract class PercentageInterface {
   double get maximumDiscountValue;
   set maximumDiscountValue(double value);
-
-  // Widget detailsWidget(BuildContext context) {
-  //   return Text(
-  //     'Maximum discount $maximumDiscountValue\$',
-  //     style: AppTextStyle.regularText,
-  //   );
-  // }
 }
