dc69aa02ac7c5acd9c7bec898deff6c5749e8052
Đỗ Hồng Quân <148946010+quandohong109@users.noreply.github.com>
2025-10-13 16:19:32 +0700

    Create new_enums.dart

---
+++ lib/data/new_data/new_enums.dart
@@ -0,0 +1,110 @@
+enum Socket {
+    lga1700('LGA 1700'),
+    lga1200('LGA 1200'),
+    lga1151('LGA 1151'),
+    lga1150('LGA 1150'),
+    am4('AM4'),
+    am5('AM5'),
+    tr4('TR4'),
+    sTRX4('sTRX4'),
+    sWRX8('sWRX8'),
+    sp3('SP3'),
+    sp5('SP5');
+
+  final String description;
+
+  const Socket(this.description);
+
+  String getName() {
+    return name;
+  }
+
+  @override
+  String toString() {
+    return description;
+  }
+}
+
+extension SocketExtension on Socket {
+  static Socket fromName(String name) {
+    return Socket.values.firstWhere((e) => e.getName() == name);
+  }
+}
+
+enum DriveFormFactor {
+    m2_2280('M.2 2280'),
+    inch3_5('3.5"'),
+    inch2_5('2.5"');
+    
+    final String description;
+    
+    const DriveFormFactor(this.description);
+    
+    String getName() {
+        return name;
+    }
+    
+    @override
+    String toString() {
+        return description;
+    }
+}
+
+extension DriveFormFactorExtension on DriveFormFactor {
+    static DriveFormFactor fromName(String name) {
+        return DriveFormFactor.values.firstWhere((e) => e.getName() == name);
+    }
+}
+
+enum InterfaceType {
+    sata('SATA'),
+    pcie('PCIe');
+
+  final String description;
+
+  const InterfaceType(this.description);
+
+  String getName() {
+    return name;
+  }
+
+  @override
+  String toString() {
+    return description;
+  }
+}
+
+extension InterfaceTypeExtension on InterfaceType {
+  static InterfaceType fromName(String name) {
+    return InterfaceType.values.firstWhere((e) => e.getName() == name);
+  }
+}
+
+enum VramVersion {
+    gddr4('GDDR4'),
+    gddr5('GDDR5'),
+    gddr5x('GDDR5X'),
+    gddr6('GDDR6'),
+    gddr6x('GDDR6X'),
+    gddr7('GDDR7'),
+    gddr7x('GDDR7X');
+
+  final String description;
+
+  const VramVersion(this.description);
+
+  String getName() {
+    return name;
+  }
+
+  @override
+  String toString() {
+    return description;
+  }
+}
+
+extension VramVersionExtension on VramVersion {
+  static VramVersion fromName(String name) {
+    return VramVersion.values.firstWhere((e) => e.getName() == name);
+  }
+}
